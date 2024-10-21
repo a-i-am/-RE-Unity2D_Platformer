33e3ea35145f55092ac0c3e2fe3f931e70563cac
a-i-am <sien6918@daum.net>
2024-10-22 01:55:34 +0900

    2024-10-22(화) 임시 백업
    - 씬 : 프롤로그 1
    -- 타임라인 컷씬 제작 중
    PlayableAPI 및 커스텀 트랙으로 다이얼로그 시스템과 타임라인 연결 중임
    -- 다이얼로그 시스템은 기존에 틀을 구현해두었고, 다이얼로그와 함께 재생되어야할 애니메이션들을 연결해야함.
    --- 캐릭터들의 대화가 진행되는 동안 플레이어의 입력 동작 전까진 타임라인 애니메이션과 다음 대화로 넘어가지 않게 하려고 함.
    - 타임라인 중지. nextDialouge() 실행 시 타임라인 이어서 재생.
    또 다음 분기 바로직전까지만 타임라인 유지. nextDialouge() 실행 시 타임라인 이어서 재생.
    이 로직대로 적용할 생각.
    -- 특정 애니메이션 지점에선 대화 넘어가기 전까지 루프 동작도 적용할 예정임(프롤로그 1 대화 타임라인 마지막에 Actor들이 다같이 날뛰는 동작)

--- Assets/CutScene/Cutscene.cs
+++ Assets/CutScene/Cutscene.cs
@@ -9,27 +9,27 @@ using UltEvents;
 
 public class Cutscene : MonoBehaviour
 {
-[System.Serializable]
-public class CutscenePart
-{
-     public PlayableDirector playableDir;
-     public DirectorWrapMode wrapMode = DirectorWrapMode.None;
-     public bool continueDialogue = false;
-     public bool waitForFixedUpdateAfterStop = false;
-}
+    [System.Serializable]
+    public class CutscenePart
+    {
+        public PlayableDirector playableDir;
+        public DirectorWrapMode wrapMode = DirectorWrapMode.None;
+        public bool continueDialogue = false;
+        public bool waitForFixedUpdateAfterStop = false;
+    }
 
-[System.Serializable]
-public class DialougeArea
-{
-    [TextArea]
-    public string dialogue;
-    public Sprite actorIcon;
-    //public Sprite iconBack;
-    public string actorName;
-    public string title;
-}
+    [System.Serializable]
+    public class DialougeArea
+    {
+        [TextArea]
+        public string dialogue;
+        public Sprite actorIcon;
+        public string actorName;
+        public string title;
+    }
+
     // UltEvent
-    public UltEvent<Cutscene> OnStart; // UltEvent is similar to UnityEvent but with more features
+    public UltEvent<Cutscene> OnStart;
     public UltEvent<Cutscene> OnStop;
     public UltEvent<Cutscene> OnComplete;
 
@@ -47,36 +47,37 @@ public class DialougeArea
     public static Cutscene ActiveCutscene;
     public List<CutscenePart> cutsceneParts = new List<CutscenePart>();
     Tuple<float, float> loop;
-    
-    //Property
-
-    //읽기 전용 프로퍼티
-    //  현재 컷신의 실행 중 여부 확인
-
-    public bool IsCurrentCutScene
-    {
-        get
-        {
-            return playableDir.playableGraph.IsValid() && playableDir.playableGraph.IsPlaying(); ;
-        }
-    }
 
     [SerializeField] private PlayableDirector playableDir;
+
     public virtual void Awake()
     {
         playableDir = GetComponent<PlayableDirector>();
         playableDir.played += (dir) =>
         {
             if (dir == playableDir)
+            {
                 ActiveCutscene = this;
+                Debug.Log("PlayableDirector 재생됨: ActiveCutscene 할당 완료");
+            }
+        };
+
+        playableDir.stopped += (dir) =>
+        {
+            if (dir == playableDir)
+            {
+                Debug.Log("PlayableDirector 중지됨");
+            }
         };
     }
 
     void FixedUpdate()
     {
-        if(loop != null && playableDir.time > loop.Item2){
+        if (loop != null && playableDir.time > loop.Item2)
+        {
             playableDir.time = loop.Item1;
-        }    
+            Debug.Log($"타임라인 루프 중: {loop.Item1}초에서 {loop.Item2}초로 돌아감");
+        }
     }
 
     public void Loop(float start, float end, bool withOffset = true)
@@ -84,12 +85,14 @@ public class DialougeArea
         start = withOffset ? start : start + (float)playableDir.time;
         end = withOffset ? end : end + (float)playableDir.time;
         loop = new Tuple<float, float>(start, end);
+        Debug.Log($"타임라인 루프 시작: 시작 {start}초, 끝 {end}초");
     }
 
     void Start()
     {
         OnOff(false);
     }
+
     void Update()
     {
         if (isDialogue)
@@ -100,12 +103,10 @@ public class DialougeArea
                     NextDialogue();
                 else
                     OnOff(false);
-
             }
         }
     }
 
-    // 대화창 띄움/넘김
     public void ShowDialogue()
     {
         OnOff(true);
@@ -113,6 +114,7 @@ public class DialougeArea
         isDialogue = true;
         NextDialogue();
     }
+
     private void NextDialogue()
     {
         txt_Dialogue.text = dialougeArea[count].dialogue;
@@ -132,6 +134,7 @@ public class DialougeArea
         }
         count++;
     }
+
     private void OnOff(bool _flag)
     {
         image_DialougeBox.gameObject.SetActive(_flag);
@@ -143,35 +146,40 @@ public class DialougeArea
         isDialogue = _flag;
     }
 
-// 타임라인 
-
-    // 타임라인 일시정지
+    // 타임라인 관련 메서드들
     public void Pause()
     {
         playableDir.playableGraph.GetRootPlayable(0).SetSpeed(0);
+        Debug.Log("PlayableDirector 일시정지됨");
     }
 
-    //  타임라인의 루프(구간 반복) 중지
     public void EndLoop()
     {
         loop = null;
+        Debug.Log("타임라인 루프 중지");
     }
 
-    // 타임라인 이어서 재생 (처음부터 재생 x, 멈춘 부분부터 재생)
     public void Continue()
     {
         playableDir.playableGraph.GetRootPlayable(0).SetSpeed(1);
         EndLoop();
+        Debug.Log("타임라인 이어서 재생됨");
     }
 
-    // 타임라인 재생 종료(초기화)
     public void ForceEnd()
     {
         if (IsCurrentCutScene)
         {
             playableDir.playableGraph.Stop();
+            Debug.Log("타임라인 강제 종료됨");
         }
     }
 
-
+    public bool IsCurrentCutScene
+    {
+        get
+        {
+            return playableDir.playableGraph.IsValid() && playableDir.playableGraph.IsPlaying();
+        }
+    }
 }
--- Assets/CutScene/DialogueLoopPlayableBehaviour.cs
+++ Assets/CutScene/DialogueLoopPlayableBehaviour.cs
@@ -7,25 +7,49 @@ public class DialogueLoopPlayableBehaviour : PlayableBehaviour
 {
     public CutscenePauseMethod pauseMethod = CutscenePauseMethod.Pause;
     bool firstFrame = true;
+
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
         if (firstFrame)
         {
+            Debug.Log("ProcessFrame called. Pause Method: " + pauseMethod);
+
             switch (pauseMethod)
             {
                 case CutscenePauseMethod.Loop:
-                    var start = 0;
-                    var end = start + playable.GetDuration();
-                    Cutscene.ActiveCutscene?.Loop((float)start, (float)end, withOffset: false);
+                    var start = 0f;
+                    var end = (float)playable.GetDuration();  // end를 playable의 duration 값으로 설정
+                    Debug.Log($"Looping from start: {start} to end: {end}");
+
+                    if (Cutscene.ActiveCutscene != null)
+                    {
+                        Cutscene.ActiveCutscene.Loop(start, end, withOffset: false);
+                        Debug.Log("Cutscene loop activated.");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Cutscene.ActiveCutscene is null.");
+                    }
                     break;
+
                 case CutscenePauseMethod.Pause:
-                    Cutscene.ActiveCutscene?.Pause();
+                    if (Cutscene.ActiveCutscene != null)
+                    {
+                        Cutscene.ActiveCutscene.Pause();
+                        Debug.Log("Cutscene paused.");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Cutscene.ActiveCutscene is null.");
+                    }
                     break;
+
                 default:
+                    Debug.LogWarning("Unknown pause method.");
                     break;
             }
-            //DialoguePopup.instance?.TryToContinue();
-            firstFrame = false;
+
+            firstFrame = false;  // 첫 프레임이 실행된 후에는 false로 설정
         }
     }
 }
