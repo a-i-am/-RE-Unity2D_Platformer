077dfde4665bd5664a3c04917caac4eac1e0de44
a-i-am <sien6918@daum.net>
2024-10-22 18:50:21 +0900

    2024-10-22(화)
    - 프롤로그 1 컷씬 타임라인 작업완료

--- Assets/CutScene/Cutscene.cs
+++ Assets/CutScene/Cutscene.cs
@@ -6,6 +6,8 @@ using TMPro;
 using UnityEngine.Playables;
 using System;
 using UltEvents;
+using UnityEngine.Timeline;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Cutscene : MonoBehaviour
 {
@@ -32,7 +34,7 @@ public class Cutscene : MonoBehaviour
     public UltEvent<Cutscene> OnStart;
     public UltEvent<Cutscene> OnStop;
     public UltEvent<Cutscene> OnComplete;
-
+    bool isResumeClip;
     [SerializeField] private DialougeArea[] dialougeArea;
     [SerializeField] private Image image_ActorIcon;
     [SerializeField] private Image image_IconBack;
@@ -40,6 +42,8 @@ public class Cutscene : MonoBehaviour
     [SerializeField] private TextMeshProUGUI txt_TitleText;
     [SerializeField] private TextMeshProUGUI txt_ActorName;
     [SerializeField] private TextMeshProUGUI txt_Dialogue;
+
+
     private bool isDialogue = false;
     private int count = 0;
 
@@ -49,15 +53,28 @@ public class Cutscene : MonoBehaviour
     Tuple<float, float> loop;
 
     [SerializeField] private PlayableDirector playableDir;
+    public TimelineAsset timeline;
 
+    void Start()
+    {
+        OnOff(false);
+
+        // pause
+        GetComponent<PlayableDirector>().playableGraph.GetRootPlayable(0).SetSpeed(0);
+        // play
+        GetComponent<PlayableDirector>().playableGraph.GetRootPlayable(0).SetSpeed(1);
+    }
     public virtual void Awake()
     {
         playableDir = GetComponent<PlayableDirector>();
+
+        ActiveCutscene = this;
+
         playableDir.played += (dir) =>
         {
             if (dir == playableDir)
             {
-                ActiveCutscene = this;
+                //ActiveCutscene = this;
                 Debug.Log("PlayableDirector 재생됨: ActiveCutscene 할당 완료");
             }
         };
@@ -69,6 +86,12 @@ public class Cutscene : MonoBehaviour
                 Debug.Log("PlayableDirector 중지됨");
             }
         };
+
+        // PlayableGraph의 상태를 확인
+        if (!playableDir.playableGraph.IsValid() || !playableDir.playableGraph.IsPlaying())
+        {
+            playableDir.Play(); // PlayableDirector를 재생하여 ActiveCutscene 설정
+        }
     }
 
     void FixedUpdate()
@@ -88,51 +111,66 @@ public class Cutscene : MonoBehaviour
         Debug.Log($"타임라인 루프 시작: 시작 {start}초, 끝 {end}초");
     }
 
-    void Start()
-    {
-        OnOff(false);
-    }
 
     void Update()
     {
         if (isDialogue)
         {
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 if (count < dialougeArea.Length)
+                {
+                    Continue();
                     NextDialogue();
+                }
                 else
                     OnOff(false);
             }
         }
     }
 
+
+
+
     public void ShowDialogue()
     {
         OnOff(true);
-        count = 0;
+        count = 1;
         isDialogue = true;
-        NextDialogue();
+        Pause();
     }
 
-    private void NextDialogue()
+    public void NextDialogue()
     {
-        txt_Dialogue.text = dialougeArea[count].dialogue;
-        txt_TitleText.text = dialougeArea[count].title;
-        txt_ActorName.text = dialougeArea[count].actorName;
+        ResumeFromClosestClip();
+        if (isResumeClip)
+        {
+        
+            if (count < dialougeArea.Length)
+            {
+                txt_Dialogue.text = dialougeArea[count].dialogue;
+                txt_TitleText.text = dialougeArea[count].title;
+                txt_ActorName.text = dialougeArea[count].actorName;
 
-        if (string.IsNullOrEmpty(dialougeArea[count].title))
-        {
-            image_ActorIcon.sprite = dialougeArea[count].actorIcon;
-            image_IconBack.gameObject.SetActive(true);
-            image_ActorIcon.gameObject.SetActive(true);
+                if (string.IsNullOrEmpty(dialougeArea[count].title))
+                {
+                    image_ActorIcon.sprite = dialougeArea[count].actorIcon;
+                    image_IconBack.gameObject.SetActive(true);
+                    image_ActorIcon.gameObject.SetActive(true);
+                }
+                else
+                {
+                    image_ActorIcon.gameObject.SetActive(false);
+                    image_IconBack.gameObject.SetActive(false);
+                }
+                count++;
+            }
+            else
+            {
+                OnOff(false);  // 대화가 끝났을 때
+            }
         }
-        else
-        {
-            image_ActorIcon.gameObject.SetActive(false);
-            image_IconBack.gameObject.SetActive(false);
-        }
-        count++;
+
     }
 
     private void OnOff(bool _flag)
@@ -145,6 +183,50 @@ public class Cutscene : MonoBehaviour
         txt_ActorName.gameObject.SetActive(_flag);
         isDialogue = _flag;
     }
+
+
+
+    // 타임라인에서 가장 가까운 애니메이션 클립으로 이동해서 이어서 재생
+    public void ResumeFromClosestClip()
+    {
+        isResumeClip = true;
+        double currentTime = playableDir.time;
+        TimelineClip closestClip = null;
+        double closestStartTime = double.MaxValue;
+
+        // 타임라인에 있는 트랙을 순회하며 가장 가까운 애니메이션 클립을 찾음
+        foreach (var track in timeline.GetOutputTracks())
+        {
+            foreach (TimelineClip clip in track.GetClips())
+            {
+                // 현재 시간보다 뒤에 있는 클립 중 가장 가까운 것을 찾음
+                if (clip.start > currentTime && clip.start < closestStartTime)
+                {
+                    closestClip = clip;
+                    closestStartTime = clip.start;
+                }
+            }
+        }
+
+        if (closestClip != null)
+        {
+            Debug.Log($"Resuming from closest clip at time {closestClip.start}");
+            playableDir.time = closestClip.start; // 가장 가까운 클립의 시작점으로 타임라인 시간을 설정
+            playableDir.Play(); // 타임라인 재생
+        }
+        else
+        {
+            Debug.Log("No upcoming clip found.");
+        }
+    }
+
+
+
+
+
+
+
+
 
     // 타임라인 관련 메서드들
     public void Pause()
