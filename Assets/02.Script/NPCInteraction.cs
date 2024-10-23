4995304423b9e58c72c8f2b70bf6c7ee429111a5
a-i-am <sien6918@daum.net>
2024-10-23 15:08:58 +0900

    2024-10-23(수) 임시 백업 3
    - 프롤로그-1 씬 방어막 이펙트 연출 추가

--- Assets/02.Script/NPCInteraction.cs
+++ Assets/02.Script/NPCInteraction.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class NPCInteraction : MonoBehaviour
 {
     Animator[] animators;
+    [SerializeField] GameObject barrier;
+    [SerializeField] GameObject barrierVFX;
 
+    private bool barrierCreated;
     void Start()
     {
         // 현 개체와 모든 자식들의 Animator 컴포넌트를 가져옴
@@ -14,15 +17,27 @@ public class NPCInteraction : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Player" && !barrierCreated)
         {
-            // 모든 Animator에 대해 IsActing 값을 true로 설정
-            foreach (Animator animator in animators)
-            {
-                animator.SetBool("IsActing", true);
-            }
-
+            barrierCreated = true;
+            Instantiate(barrierVFX, null); // 부모를 null로 설정하여 씬의 루트에 생성
+            Invoke("InstantiateBarrier", 1.2f);
+            Invoke("ActAnimation", 1.2f);
         }
+    }
+
+    void ActAnimation()
+    {
+        // 모든 Animator에 대해 IsActing 값을 true로 설정
+        foreach (Animator animator in animators)
+        {
+            animator.SetBool("IsActing", true);
+        }
+    }
+
+    void InstantiateBarrier()
+    {
+        Instantiate(barrier, null); // 부모를 null로 설정하여 씬의 루트에 생성
     }
 
 
